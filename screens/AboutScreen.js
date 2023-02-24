99617b0473c82792b7aeb8db9db5551bcb19e5f1
Christa <jaysplaypen@gmail.com>
2023-02-23 16:28:30 -0800

    Activity Indicator

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -2,6 +2,8 @@ import { useSelector } from "react-redux";
 import { ScrollView, Text } from "react-native";
 import { Card, ListItem, Avatar } from "react-native-elements";
 import { baseUrl } from "../shared/baseUrl";
+import Loading from '../components/LoadingComponent';
+
 
 
 
@@ -21,6 +23,31 @@ const Mission = () => {
 const AboutScreen = () => {
     const partners = useSelector((state) => state.partners);
 
+    if (partners.isLoading) {
+        return (
+            <ScrollView>
+                <Mission />
+                <Card>
+                    <Card.Title>Community Partners</Card.Title>
+                    <Card.Divider />
+                    <Loading />
+                </Card>
+            </ScrollView>
+        );
+    }
+    if (partners.errMess) {
+        return (
+            <ScrollView>
+                <Mission />
+                <Card>
+                    <Card.Title>Community Partners</Card.Title>
+                    <Card.Divider />
+                    <Text>{partners.errMess}</Text>
+                </Card>
+            </ScrollView>
+        );
+    }
+    
     return (
         <ScrollView>
             <Mission />
